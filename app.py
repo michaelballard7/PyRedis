b5fff80e2defd4715bc521c249e276df969ef564
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro-2.local>
2019-01-07 20:58:01 -0500

    added scraping to store practice data

--- app.py
+++ app.py
@@ -1,12 +1,62 @@
 """ Remember to install and activate the redis server first """
 
+# https://stackoverflow.com/questions/8810036/complex-data-structures-redis
+# https://redis-py.readthedocs.io/en/latest/_modules/redis/client.html#Redis.hmset
+
 import redis
-import requests as r
-import bs4 as soup
+import requests as resp
+from bs4 import BeautifulSoup as bs
+import json
+import pprint
+
+
+# header values to bypass site filters
+headerValues = {
+     "User-Agent" : "Michael Ballard App / 1.0.0"
+    }
+
+# url for scraping
+url = "https://www.basketball-reference.com/teams/DEN/"
+
 
 # establish the database connection
 r = redis.Redis(host='localhost', port=6379, db=0)
 
-r.set('foo',"bar")
+# request and response object
+response = resp.get(url, headers=headerValues)
 
-print(r.get('foo'))
+# raw response text stripped of spaces
+data = response.text.strip()
+
+# create soup object to access bs4 methods
+soup = bs(data, 'html.parser')
+
+# use bs4 find method to locate table element on page
+table = soup.find(class_="sortable stats_table")
+
+# find the table body element
+table_body = soup.tbody
+
+# iterate throught t body to findAll rows
+for row in table_body.findAll("tr"):
+    for td in row.findAll("td"):
+            print(td)
+    
+dicty = {"pg":"D Russell", "sg": "m ballard", "sf":"lawerence", "pf":"jath", "c":"malorie"}
+
+# r.set('foo',"bar")
+# print(r.get('foo'))
+
+# r.mset(dicty)
+# print("Brooklyn Players: {}".format((r.mget("pg", "c"))))
+
+
+# print("The key pg",r.get('pg'))
+
+# r.hmset("team13", "pg" "D Russell" "sg" "M ballard" )
+# print(r.hgetall("team13"))
+
+# r.hmset("brooklyn", dicty)
+# print(r.hgetall("brooklyn"))
+
+# print(r.keys())
